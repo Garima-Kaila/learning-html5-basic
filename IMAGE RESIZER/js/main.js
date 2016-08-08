1e4c2176f4e0e4a8514776e1a03eba3a9e89f0c4
Garima Kaila <garimakaila03@gmail.com>
2016-08-09 00:13:25 +0530

    Modified for repositioning

--- IMAGE RESIZER/js/main.js
+++ IMAGE RESIZER/js/main.js
@@ -9,7 +9,9 @@ imgCircle.src = "./images/circle.png";
 var imgRect = new Image();
 imgRect.src = "./images/rect.png";
 
-
+var resizerRadius = 2;
+var rr = resizerRadius * resizerRadius;
+var draggingImage = false;
 var shapes = [];
 function drawCircle() {
     var circle = new Shape();
@@ -19,7 +21,7 @@ function drawCircle() {
     circle.h = 100 * Math.random();
     circle.img = imgCircle;
     shapes.push(circle);
-    circle.draw();
+    circle.draw(true);
 
 }
 
@@ -31,7 +33,7 @@ function drawRectangle() {
     rect.h = 100 * Math.random();
     rect.img = imgRect;
     shapes.push(rect);
-    rect.draw();
+    rect.draw(true);
 }
 
 
@@ -40,6 +42,7 @@ function drawRectangle() {
 canvas.addEventListener("mousedown", getPosition, false);
 
 var xPost, yPost;
+var shape;
 function getPosition(event) {
     xPost = new Number();
     yPost = new Number();
@@ -62,21 +65,101 @@ function getPosition(event) {
 
     console.log("x: " + xPost + "  y: " + yPost);
     var shapeOnWhichBorderIsShown = null;
-//ctx.clearRect(0,0,canvas.width,canvas.height);
-    for (var idx = 0; idx <shapes.length; idx++) {  //c1,c2,c3
-        var shape = shapes[idx];
+    //ctx.clearRect(0,0,canvas.width,canvas.height);
+    for (var idx = 0; idx < shapes.length; idx++) {  //c1,c2,c3
+        shape = shapes[idx];
         if (shape.amIClicked(xPost, yPost) && shapeOnWhichBorderIsShown == null) {
             shapeOnWhichBorderIsShown = shape;  //c3
         }
-            shape.clearBorder();
-            shape.draw();
+        shape.clearBorder();
+        shape.draw(true);
     }
 
-    if(shapeOnWhichBorderIsShown!=null){
+    if (shapeOnWhichBorderIsShown != null) {
         shapeOnWhichBorderIsShown.createBorder();
     }
 
+}
 
-    // alert("x: " + xPost + "  y: " + yPost);
+
+var offsetX = canvas.offsetLeft;
+var offsetY = canvas.offsetTop;
+var startX, startY;
+var draggingResizer = {
+    x: 0,
+    y: 0
+};
+
+
+function drawDragAnchor(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, resizerRadius, 0, 2 * Math.PI, false);
+    ctx.closePath();
+    ctx.fill();
+}
+function handleMouseDown(e) {
+    startX = parseInt(e.clientX - offsetX);
+    startY = parseInt(e.clientY - offsetY);
+    draggingResizer = shape.anchorHitTest(startX, startY);
+    draggingImage = draggingResizer < 0 && shape.hitImage(startX, startY);
+}
+
+
+function handleMouseMove(e) {
+
+    if (draggingResizer > -1) {
+
+        mouseX = parseInt(e.clientX - offsetX);
+        mouseY = parseInt(e.clientY - offsetY);
+        var imgright, imgbottom;
+        shape.resizeShapeFunc(mouseX, mouseY);
+
+        // redraw the image with resizing anchors
+        shape.draw(true);
+
+    } else if (draggingImage) {
+
+        imageClick = false;
+
+        mouseX = parseInt(e.clientX - offsetX);
+        mouseY = parseInt(e.clientY - offsetY);
+
+        // move the image by the amount of the latest drag
+        var dx = mouseX - startX;
+        var dy = mouseY - startY;
+        shape.imageResizeDrag(dx, dy);
+        // reset the startXY for next time
+        startX = mouseX;
+        startY = mouseY;
+
+        // redraw the image
+        shape.draw(false);
+
+    }
+
+}
+
+function handleMouseUp(e) {
+    draggingResizer = -1;
+    draggingImage = false;
+    shape.draw(true);
+}
+
+function handleMouseOut(e) {
+    handleMouseUp(e);
+}
+
+
+document.body.onmousedown = function (e) {
+    handleMouseDown(e);
+}
+document.body.onmousemove = function (e) {
+    handleMouseMove(e);
+}
+document.body.onmouseup = function (e) {
+    handleMouseUp(e);
+}
+document.body.onmouseout = function (e) {
+    handleMouseOut(e);
 }
 
--- IMAGE RESIZER/js/shape.js
+++ IMAGE RESIZER/js/shape.js
@@ -1,23 +1,128 @@
-/**
- * Created by garima05 on 07-08-2016.
- */
-function Shape() {
-    this.x = 0;
-    this.y = 0;
-    this.w = 0;
-    this.h = 0;
-    this.img = 0;
-}
+        /**
+         * Created by garima05 on 07-08-2016.
+         */
+        function Shape() {
+            this.x = 0;
+            this.y = 0;
+            this.w = 0;
+            this.h = 0;
+            this.img = 0;
+        }
 
-Shape.prototype.draw = function () {
-    ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-};
-Shape.prototype.createBorder = function () {
-    ctx.strokeRect(this.x, this.y, this.w, this.h);
-};
-Shape.prototype.amIClicked = function (xPost, yPost) {
-    return (xPost > this.x && xPost < (this.x + this.w) && yPost > this.y && yPost < (this.y + this.h))
-};
-Shape.prototype.clearBorder = function () {
-    ctx.clearRect(this.x-1, this.y-1, this.w+2, this.h+2);
-};
+        // imageRight = imageX + imageWidth;
+        //imageBottom = imageY + imageHeight
+
+        Shape.prototype.draw = function (withAnchors) {
+
+            //  ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+
+            if (withAnchors) {
+                drawDragAnchor(this.x, this.y);
+                drawDragAnchor((this.x + this.w), this.y);
+                drawDragAnchor((this.x + this.w), (this.y + this.h));
+                drawDragAnchor(this.x, (this.y + this.h));
+            }
+        };
+
+        //function drawDragAnchor(x, y) {
+        //    ctx.beginPath();
+        //    ctx.arc(x, y, resizerRadius, 0, 2 * Math.PI, false);
+        //    ctx.closePath();
+        //    ctx.fill();
+        //}
+
+        Shape.prototype.createBorder = function () {
+            ctx.strokeRect(this.x, this.y, this.w, this.h);
+        };
+        Shape.prototype.amIClicked = function (xPost, yPost) {
+            return (xPost > this.x && xPost < (this.x + this.w) && yPost > this.y && yPost < (this.y + this.h))
+        };
+        Shape.prototype.clearBorder = function () {
+            ctx.clearRect(this.x - 1, this.y - 1, this.w + 2, this.h + 2);
+        };
+
+        Shape.prototype.hitImage = function (startX, startY) {
+            return (startX > this.x && startX < this.x + this.w && startY > this.y && startY < this.y + this.h);
+        };
+
+        Shape.prototype.anchorHitTest = function (startX, startY) {
+            var dx, dy;
+
+            // top-left
+            dx = startX - this.x;
+            dy = startY - this.y;
+            if (dx * dx + dy * dy <= rr) {
+                return (0);
+            }
+            // top-right
+            dx = startX - (this.x + this.w);
+            dy = startY - this.y;
+            if (dx * dx + dy * dy <= rr) {
+                return (1);
+            }
+            // bottom-right
+            dx = startX - (this.x + this.w);
+            dy = startY - (this.y + this.h);
+            if (dx * dx + dy * dy <= rr) {
+                return (2);
+            }
+            // bottom-left
+            dx = startX - this.x;
+            dy = startY - (this.y + this.h);
+            if (dx * dx + dy * dy <= rr) {
+                return (3);
+            }
+            return (-1);
+        };
+
+        Shape.prototype.resizeShapeFunc = function (mouseX, mouseY) {
+
+        // resize the image
+            switch (draggingResizer) {
+                case 0:
+                    //top-left
+                    this.x = mouseX;
+                    this.w = (this.x + this.x) - mouseX;
+                    this.y = mouseY;
+                    this.h = (this.y + this.h) - mouseY;
+                    break;
+                case 1:
+                    //top-right
+                    this.y = mouseY;
+                    this.w = mouseX - this.x;
+                    this.h = (this.y + this.h) - mouseY;
+                    break;
+                case 2:
+                    //bottom-right
+                    this.w = mouseX - this.x;
+                    this.h = mouseY - this.y;
+                    break;
+                case 3:
+                    //bottom-left
+                    this.x = mouseX;
+                    this.w = (this.x + this.w) - mouseX;
+                    this.h = mouseY - this.y;
+                    break;
+            }
+
+            if (this.w < 25) {
+                this.w = 25;
+            }
+            if (this.h < 25) {
+                this.h = 25;
+            }
+
+        // set the image right and bottom
+            imgright = this.x + this.w;
+            imgbottom = this.y + this.h;
+        };
+
+
+        Shape.prototype.imageResizeDrag = function (dx, dy) {
+            this.x += dx;
+            this.y += dy;
+            this.r += dx;
+            this.h += dy;
+
+        };
